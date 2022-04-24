83fe633a1f931226ed63e5c990d9e6f6333f3296
Shir Bar Lev <shirblc@gmail.com>
2022-04-24 21:00:30 +0300

    Initialised the data controller

--- happierReminders/AppDelegate.swift
+++ happierReminders/AppDelegate.swift
@@ -9,7 +9,7 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    let dataManager = DataManager()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -24,12 +24,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        dataManager.saveAllContexts()
     }
 
+    func applicationWillTerminate(_ application: UIApplication) {
+        dataManager.saveAllContexts()
+    }
 }
 
