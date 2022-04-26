044b13921afc7457c7f340e479def8290ae72ca5
Shir Bar Lev <shirblc@gmail.com>
2022-04-26 17:05:57 +0300

    Added second view controller for quotes

---
+++ happierReminders/ViewControllers/QuotesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  QuotesViewController.swift
+//  happierReminders
+//
+//  Created by Shir Bar Lev on 26/04/2022.
+//
+
+import UIKit
+
+class QuotesViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
