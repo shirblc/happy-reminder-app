34415821e3f2b9b75921eecb270f439f6184b15c
Shir Bar Lev <shirblc@gmail.com>
2022-05-03 10:40:43 +0300

    Set up QuoteViewController class

---
+++ happierReminders/ViewControllers/QuotesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesViewController.swift
+//  happierReminders
+//
+//  Created by Shir Bar Lev on 03/05/2022.
+//
+
+import UIKit
+
+class QuotesViewController: UIViewController {
+    var dataManager: DataManager!
+    var collection: Collection!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
