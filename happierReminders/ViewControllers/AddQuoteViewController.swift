69f435d1bbb83408b80762f5080e7d03a319c93e
Shir Bar Lev <shirblc@gmail.com>
2022-05-04 20:50:29 +0300

    Added initial view controller for adding quotes

---
+++ happierReminders/ViewControllers/AddQuoteViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddQuoteViewController.swift
+//  happierReminders
+//
+//  Created by Shir Bar Lev on 04/05/2022.
+//
+
+import UIKit
+
+class AddQuoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
