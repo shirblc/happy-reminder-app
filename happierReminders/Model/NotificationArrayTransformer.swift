06337988c0d74ea859b42ae891198663cabb399c
Shir Bar Lev <shirblc@gmail.com>
2022-05-06 21:34:50 +0300

    Added new properties for notifications days & time

---
+++ happierReminders/Model/NotificationArrayTransformer.swift
@@ -0,0 +1,19 @@
+//
+//  NotificationArrayTransformer.swift
+//  happierReminders
+//
+//  Created by Shir Bar Lev on 06/05/2022.
+//
+
+import Foundation
+import CoreData
+
+final class NotificationArrayTransformer: NSSecureUnarchiveFromDataTransformer {
+    override static var allowedTopLevelClasses: [AnyClass] {
+        return [NSString.self]
+    }
+    
+    public static func register() {
+        ValueTransformer.setValueTransformer(NotificationArrayTransformer(), forName: NSValueTransformerName(rawValue: "NotificationArrayTransformer"))
+    }
+}
