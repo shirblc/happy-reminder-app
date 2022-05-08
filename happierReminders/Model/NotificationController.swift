8ebfeb881dd39b840dcd0f63175927df0f0c6ebe
Shir Bar Lev <shirblc@gmail.com>
2022-05-08 17:23:49 +0100

    Added functionality for scheduling notifications

--- happierReminders/Model/NotificationController.swift
+++ happierReminders/Model/NotificationController.swift
@@ -8,8 +8,16 @@
 import Foundation
 import UserNotifications
 
+struct UserNotificationData {
+    let daysOfWeek: [Int]
+    let time: String
+    let quoteType: String
+    let quoteText: String
+    let collectionID: String
+}
+
 class NotificationController {
-    var authorisedNotifications: Bool? = nil
+    var scheduledNotificationIdentifiers: [String: [String]] = [:]
     let notificationCentre = UNUserNotificationCenter.current()
     static let shared = NotificationController()
     
@@ -50,7 +58,7 @@ class NotificationController {
     
     // buildNotificationRequest
     // Builds the UNNotificationRequest to schedule
-    func buildNotificationRequest(daysOfWeek: [Int], time: String, quoteText: String, quoteType: String) -> [UNNotificationRequest] {
+    private func buildNotificationRequest(daysOfWeek: [Int], time: String, quoteText: String, quoteType: String) -> [UNNotificationRequest] {
         // Set the notification's content
         let notificationContent = UNMutableNotificationContent()
         notificationContent.title = "\(quoteType) Reminder:"
@@ -73,7 +81,32 @@ class NotificationController {
     
     // scheduleNotifications
     // Schedules notifications based on the user's settings
-    func scheduleNotifications() {
-        // TODO
+    func scheduleNotifications(notificationsData: UserNotificationData, permissionDeniedHandler: @escaping () -> Void, errorHandler: @escaping (Error) -> Void) {
+        Task(priority: .medium) {
+            let authorisedNotifications = await getNotificationsAuthorisationStatus(errorHandler: errorHandler)
+            
+            // make sure we have permission to send notifications
+            guard authorisedNotifications else {
+                permissionDeniedHandler()
+                return
+            }
+            
+            let notificationRequests = buildNotificationRequest(daysOfWeek: notificationsData.daysOfWeek, time: notificationsData.time, quoteText: notificationsData.quoteType, quoteType: notificationsData.quoteText)
+            
+            // add the requests and keep track of sent notification IDs
+            for request in notificationRequests {
+                do {
+                    try await notificationCentre.add(request)
+                    
+                    if(scheduledNotificationIdentifiers.keys.contains(notificationsData.collectionID)) {
+                        scheduledNotificationIdentifiers[notificationsData.collectionID]?.append(request.identifier)
+                    } else {
+                        scheduledNotificationIdentifiers[notificationsData.collectionID] = [request.identifier]
+                    }
+                } catch {
+                    errorHandler(error)
+                }
+            }
+        }
     }
 }
