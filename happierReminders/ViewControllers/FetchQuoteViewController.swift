849fc6191f59403311c53d026d3093b5c5eec4dd
Shir Bar Lev <shirblc@gmail.com>
2022-05-09 12:00:16 +0100

    Added initial VC for fetching quotes from the network

---
+++ happierReminders/ViewControllers/FetchQuoteViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FetchQuoteViewController.swift
+//  happierReminders
+//
+//  Created by Shir Bar Lev on 09/05/2022.
+//
+
+import UIKit
+
+class FetchQuoteViewController: UIViewController {
+    @IBOutlet weak var typeSelect: Select!
+    @IBOutlet weak var quoteTextView: UITextView!
+    @IBOutlet weak var quoteSourceView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
